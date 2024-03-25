1ee656c290456dc9d027aee79a8853a959b08386
Kim <harryqas94@gmail.com>
2024-03-25 17:26:28 +0900

    add q17219

--- 김동현/TestJava.java
+++ 김동현/TestJava.java
@@ -1,7 +1,26 @@
 package 김동현;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class TestJava {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static void main(String[] args) throws IOException {
+        // 선언 
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        // 
+        //파일에서 입력받을 경우에는 new BufferedReader(new FileReader("ex.java")); 
+
+        //라인단위로 입력받기(Enter를 경계로) 
+        String str = bf.readLine(); 
+        //정수형 입력이라면, 형변환 필수! 
+        //int i =  Integer.parseInt(bf.readLine()); 
+
+        //Space를 경계로 끊어야 할 때 
+        String arr[] = str.split(" "); //또는 StringTokenizer 클래스 이용)
+
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+
     }
 }
--- 김동현/백준/LEVEL3/q17219.java
+++ 김동현/백준/LEVEL3/q17219.java
@@ -1,5 +1,46 @@
 package 김동현.백준.LEVEL3;
 
+import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
 public class q17219 {
-    
+    public static void main(String[] args) throws IOException {
+        // 선언 
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));   //할당된 버퍼에 값 넣어주기
+
+        String str = bf.readLine(); 
+        
+        
+        //Space를 경계로 끊어야 할 때 
+        String arr[] = str.split(" ");
+
+        //int i =  Integer.parseInt(bf.readLine()); 
+
+        //총 사이트 개수와 찾아야할 사이트 개수 입력
+        int N = Integer.parseInt(arr[0]); 
+        int M = Integer.parseInt(arr[1]); 
+
+        //hashMap 사이트를 저장할 해쉬 맵
+        HashMap<String, String> siteMap = new HashMap<String, String>( );
+        
+        for(int i = 0; i < N; i++) {
+            String site = bf.readLine();
+            String siteArr[] = site.split(" ");
+
+            siteMap.put(siteArr[0], siteArr[1]);
+        }
+
+        for (int i = 0; i < M; i++) {
+            String site = bf.readLine();
+            bw.write(siteMap.get(site)+"\n");   //버퍼에 있는 값 전부 출력
+        }
+
+        bw.flush();   //남아있는 데이터를 모두 출력시킴
+        bw.close();   //스트림을 닫음
+    }
 }
